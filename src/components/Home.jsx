5a41c0f85e06a0abb51ddbc044e9c08b176d77ee
FLopez73 <flopez730@gmail.com>
2023-06-23 11:14:17 -0700

    progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BsFillArrowDownCircleFill } from "react-icons/bs"
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -13,11 +14,11 @@ const Home = () => {
                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
                 </p>
                 <div className="flex justify-center">
-                    <button className="group w-fit px-6 py-3 ">
+                    <Link to='about' smooth duration={500} className="group w-fit px-6 py-3 ">
                         <span className="group-hover:rotate-5 duration-300">
                         <BsFillArrowDownCircleFill size={30}/>
                         </span>
-                    </button> 
+                    </Link> 
                 </div>
             </div>
             <div>
