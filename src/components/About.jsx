6e865172ab8da9a62e60fecc8b3a17f126c7f821
FLopez73 <flopez730@gmail.com>
2023-07-06 17:48:33 -0700

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,9 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-        As a Full Stack Software Engineer, I am living out my lifelong passion for technology and innovation. Based in the beautiful city of San Diego, I thrive on solving complex problems with simple, elegant solutions that streamline processes and drive progress. When I'm not immersed in code, you can find me enjoying quality time with my feline companions or exploring the great outdoors through hiking, golfing, or other activities. I take pride in delivering top-quality solutions that exceed expectations, and my portfolio showcases a diverse array of projects that highlight my technical proficiency, strategic thinking, and unwavering commitment to excellence.
-        </p>
+        As a Full Stack Software Engineer, I am living out my lifelong passion for technology and innovation. Based in the beautiful city of San Diego, I thrive on solving complex problems with simple, elegant solutions that streamline processes and drive progress. I have honed my skills as a fullstack developer, specializing in creating visually appealing and responsive websites that provide a great user experience. With my strong attention to detail, excellent problem-solving skills, and ability to work effectively in a team, I am confident in my ability to deliver high-quality work on time. When I'm not immersed in code, you can find me enjoying quality time with my feline companions or exploring the great outdoors through hiking, golfing, or other activities. 
 
-     
+       </p>
       </div>
       
     </div>
