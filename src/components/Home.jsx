59bbd69a5e2851eae9bbb9ce29d479885e3c9e48
flopez73 <flopez730@gmail.com>
2024-02-12 14:41:49 -0800

    nav bar z

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
           <h2 className="text-3xl sm:text-7xl font-bold text-white">
             Hi, I am <span className='text-cyan-500'>Felix</span>, a Full Stack Web Developer.
           </h2>
-          <h3>Based in San Diego,CA</h3>
+          <h3 className='text-white'>Based in San Diego,CA</h3>
         </div>
         <div>
           <img src={Hero} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full" />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
 
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 z-2 px-4 text-white bg-black fixed">
         <div>
             <h1 className="text-5xl">F.Lopez</h1>
         </div>
